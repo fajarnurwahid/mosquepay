c8fdabd0c8522c2d1132e365536243276190f5f7
Fajar Nur Wahid <fajarnurwahid12@gmail.com>
2023-06-30 18:22:13 +0700

    updated

--- js/index.js
+++ js/index.js
@@ -5,34 +5,3 @@ AOS.init({
     offset: 24
 })
 // end: AOS
-
-
-
-// start: Services
-new Swiper('.service-swiper', {
-    spaceBetween: 24,
-    slidesPerView: 4,
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        576: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1200: {
-            slidesPerView: 4,
-        },
-    }
-})
-// end: Services
